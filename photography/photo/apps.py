ce798354c3ee3d9303756735656d47760fa51717
ElieAssaf33 <elieassaflt@gmail.com>
2025-04-23 19:37:39 +0300

    f

--- photography/photo/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class PhotoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'photo'
-
-    class Meta:
-        verbose_name = 'photos '
